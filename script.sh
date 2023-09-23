3b1607dd349959b5e6c090209c50792577398788
NRZ-7 <fabregasmj@gmail.com>
2023-09-23 14:20:14 +0200

    Adding sshd server, bugfixing and comments updated

--- script.sh
+++ script.sh
@@ -7,6 +7,8 @@
 userName=changeMe                    # Needs to match with your Debian user name.
 bash_completion_install=false   # Install and configure bash-completion for root user
 sudo_install=false              # Install and configure sudo for your Debian user name
+openssh_server_install=false    # Install and configure openssh-server
+disable_ssh_users=false         # Disable ssh login for all users EXCEPT $userName
 
 
                                         ##############
@@ -17,6 +19,7 @@ sudo_install=false              # Install and configure sudo for your Debian use
 #define current script directory
 current_dir=$(dirname "$0")
 src_dir=$current_dir"/src"
+config_dir=$current_dir"/config.d"
 
 # Define output colors
 cyan='\033[0;36m'
@@ -26,15 +29,19 @@ red='\033[1;31m'
 colorOff='\033[0m'
 
 scriptExec() {
-    scriptName="$1"
+    #Define variables
+    scriptName="$1" #$1 is a variable taked when is called scriptExec (for example scriptExec sudo)
+    #Defines the path including the script name
     scriptPath="$src_dir/$scriptName.sh"
 
     echo -e $cyan"\n Installing $scriptName...\n"$colorOff
 
     #check if scriptPath exists
     if [ -f $scriptPath ]; then
+        #if exists call the script
         source $scriptPath
     else
+        #If not exists show an error
         echo -e $red" ERROR: $scriptPath not found"$colorOff
     fi
 
@@ -116,6 +123,21 @@ if [ $sudo_install = true ]; then
 else
     echo -e $cyan"\n sudo_install disabled in config"$colorOff
 fi
+
+
+###############
+##  OPENSSH  ##
+###############
+
+# Check openssh_server_install parameter
+if [ $openssh_server_install = true ]; then
+
+    scriptExec openssh-server
+
+else
+    echo -e $cyan"\n openssh-server_install disabled in config"$colorOff
+fi
+
 
 
 # Clean unnecessary packages
---
+++ src/openssh-server.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+    # Install ssh
+    apt-get install openssh-server
+
+    #Define sshd_config file path
+    sshd_configFile="/etc/ssh/sshd_config.d/sshd_config"
+
+    # copying config file
+    cp $config_dir/sshd_config $sshd_configFile
+    echo -e $cyan"\n Copying $config_dir/sshd_config to $sshd_configFile"$colorOff
+
+
+    #Disable ssh login for all users EXCEPT $userName
+    if [ $disable_ssh_users = true ]; then
+
+        echo "" >> $sshd_configFile
+        echo "# Allow only users from this list" >> $sshd_configFile
+        echo -e "AllowUsers $userName" >> $sshd_configFile
+
+        echo -e $green"\n DONE: Disable ssh login for all users EXCEPT $userName"$colorOff
+
+    else
+        echo -e $yellow"\n Skipping: Disable ssh login for all users EXCEPT $userName is DISABLED in config"$colorOff
+    fi
