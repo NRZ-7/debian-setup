2af61f95853d486d01fffcb18f64cf6925fb7301
NRZ-7 <fabregasmj@gmail.com>
2023-12-30 02:18:24 +0100

    fixing

--- src/xfce4.sh
+++ src/xfce4.sh
@@ -1,10 +1,10 @@
 #Stop xfce4-panel using $userName user
-su $userName -c 'xfce4-panel --quit'
+sudo -u $userName xfce4-panel --quit
 #Remove current dir
 rm -r /home/$userName/.config/xfce4
 #Copy setup to config path
 cp -r $current_dir/config.d/xfce4 /home/$userName/.config/xfce4
 #Setting permissions from root to $userName
-chown -R $userName:$userName /home/$userName/.config/xfce4*
+chown -R $userName:$userName /home/$userName/.config/xfce4
 #Start xfce4-panel on $userName session
-su $userName -c 'xfce4-panel'
+sudo -u $userName xfce4-panel
