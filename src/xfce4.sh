f255a3857aa398afc070875b8ae1c4a6c87dd345
NRZ-7 <fabregasmj@gmail.com>
2024-01-08 19:40:22 +0100

    Fix xfce compatibility bug

--- src/xfce4.sh
+++ src/xfce4.sh
@@ -7,10 +7,15 @@ apt-get install -y xfce4 xfce4-goodies lightdm
 sudo -u $userName pkill -SIGTERM -f xfce4-panel
 #Remove current configuration files
 rm -r /home/$userName/.config/xfce4
-#sleep 0.2
 #Copy custom config to xfce4 username config path
 mkdir -p /home/$userName/.config && cp -r $current_dir/config.d/xfce4 /home/$userName/.config/xfce4
-#sleep 0.5
+
+#Change user in config files to $userName
+#source to undestand command: https://stackoverflow.com/questions/525592/find-and-replace-inside-a-text-file-from-a-bash-command
+
+sed -i -e 's/changeMe/'"$userName"'/g' /home/$userName/.config/xfce4/*
+
+
 #Setting permissions from root to $userName
 chown -R $userName:$userName /home/$userName/.config/xfce4
 
